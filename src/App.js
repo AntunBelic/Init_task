eb4516e5e1d558d7e38be507dbf53cc9c4444a56
AntunBelic <antunbelic93@gmail.com>
2022-05-20 14:24:34 +0200

    added context

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './Components/Login';
 import Home from './Components/Home';
+import { AuthProvider } from './Components/Auth';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/login' element={<Login/>} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
