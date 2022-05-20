70f019f3511903dd74cc9b284fcdd0b393acf0fa
AntunBelic <antunbelic93@gmail.com>
2022-05-20 19:21:16 +0200

    cleanup

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,14 @@
-import { Navigate,useNavigate} from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 import { useAuth } from './Auth'
 
 function Home() {
     const auth = useAuth();
     const navigate = useNavigate();
+
     const handleLogout = () => {
         auth.logout()
         navigate("/login")
     }
-    const authToken = JSON.parse(localStorage.getItem("token"))
-
-
   return (
     <div>
         <h1>{auth?.data?.user?.firstName}{" "}{auth?.data?.user?.lastName}</h1>
